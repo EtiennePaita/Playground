8e1b6460f0fc6b70fde4af6aa230d5efada09a3a
EtiennePaita <etiennepaita1@gmail.com>
2026-09-14 23:55:00 +0200

    [Splash] Implementation of a short animation of the app name as a Splash screen.  + Smooth shared element transition on navigate to Home

--- app/src/main/java/com/paita/playground/ui/components/AppTopBar.kt
+++ app/src/main/java/com/paita/playground/ui/components/AppTopBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.paita.playground.R
 import com.paita.playground.ui.theme.DarkGrey
 import com.paita.playground.ui.theme.Green
@@ -32,7 +30,6 @@ fun AppTopBar(
     ) {
         Column(
             modifier = Modifier
-                .padding(15.dp)
                 .align(Alignment.Center),
             horizontalAlignment = Alignment.End
         ) {
--- app/src/main/java/com/paita/playground/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/paita/playground/ui/screens/SplashScreen.kt
@@ -1,16 +1,43 @@
 package com.paita.playground.ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import com.paita.playground.R
 import com.paita.playground.ui.LocalSharedTransitionScope
 import com.paita.playground.ui.base.preview.AnimationPreviewSetup
-import com.paita.playground.ui.components.AppTopBar
+import com.paita.playground.ui.theme.DarkGrey
+import com.paita.playground.ui.theme.Green
+import com.paita.playground.ui.theme.LightBlack
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.milliseconds
 
 
 @Composable
@@ -19,25 +46,82 @@ fun SplashScreen(
     animatedVisibilityScope: AnimatedVisibilityScope,
     navigateToHome : () -> Unit,
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    var isSubtitleVisible by remember { mutableStateOf(false) }
+
+    val progress by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 700),
+        finishedListener = {
+            isSubtitleVisible = true
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        delay(300.milliseconds)
+        startAnimation = true
+    }
+
+    LaunchedEffect(isSubtitleVisible) {
+        if (isSubtitleVisible) {
+            delay(1500.milliseconds)
+            navigateToHome()
+        }
+    }
 
     with(LocalSharedTransitionScope.current) {
         val topBarSharedState = rememberSharedContentState(key = "top-app-bar-title")
         Box(
             modifier = modifier
                 .fillMaxSize()
-                .clickable {
-                    navigateToHome()
-                }
         ) {
-            AppTopBar(
-                Modifier
-                    .align(Alignment.Center)
+            Column(
+                modifier = Modifier
                     .sharedElement(
                         topBarSharedState,
                         animatedVisibilityScope
                     )
+                    .defaultMinSize(minHeight = 70.dp)
+                    .align(Alignment.Center),
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(
+                    stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge.copy(
+                        shadow = Shadow(
+                            color = DarkGrey.copy(
+                                alpha = lerp(0f,0.7f,progress) //if (progress == 1f) 0.7f else 0f
+                            ),
+                            offset = Offset(5.0f, 25.0f),
+                            blurRadius = 20f
+                        ),
+                        color = DarkGrey.copy(
+                            alpha = progress
+                        ),
+                        fontSize = lerp(18.sp, 36.sp, progress),
+                    ),
 
-            )
+                )
+                AnimatedVisibility(
+                    visible = isSubtitleVisible,
+                    enter = slideInHorizontally(
+                        animationSpec = tween(800)
+                    ) { x -> x + 10 } + fadeIn(animationSpec = tween(800)),
+                    exit = slideOutHorizontally { x -> x + 10 } + fadeOut(),
+                ) {
+                    Text(
+                        "by Etienne Païta",
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = Green,
+                            shadow = Shadow(
+                                color = LightBlack.copy(alpha = 0.3f),
+                                offset = Offset(5.0f, 20.0f),
+                                blurRadius = 8f
+                            )
+                        )
+                    )
+                }
+            }
         }
     }
 }
